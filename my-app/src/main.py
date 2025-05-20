b3ae0eaa6d56029446293e002d4f9b36815c3ad7
Luis117 <lgmm-luis14@outlook.com>
2025-05-20 15:20:38 -0600

    lo vuelvo a intentar subir a git

--- my-app/src/main.py
+++ my-app/src/main.py
@@ -1,28 +1,40 @@
 import flet as ft
 
-
 def main(page: ft.Page):
    
-    page.bgcolor = ft.colors.BLUE_GREY_50
+    page.bgcolor = ft.Colors.BLACK
     page.title = "Enciclopedia de Gamer"
     page.scroll=True
-
     titulo = ft.Text(
-        "Bienvenido a la Enciclopedia Gamer", size=30, color=ft.colors.BLACK
+        "Bienvenido a la Enciclopedia Gamer", size=30, color=ft.Colors.WHITE
     )
 
     imagen_portada = ft.Image(
-        src="https://images.hdqwalls.com/wallpapers/halo-reach-3p.jpg",
+        src="https://www.elinformador.com.co/images/stories/general/2017/06-junio/20entre6.jpg",
         width=600,
         height=200,
         fit=ft.ImageFit.CONTAIN,
     )
-    
+
+
+    inicio_container = ft.Container(
+        content=ft.Column(
+            [titulo, imagen_portada],
+            alignment=ft.MainAxisAlignment.START,
+            horizontal_alignment=ft.CrossAxisAlignment.CENTER
+        ),
+        padding=20,
+        border_radius=10,
+        bgcolor=ft.Colors.WHITE,
+    )
+
+
     tabs = ft.Tabs(
         selected_index=0,
         animation_duration=300,
         tabs=[
-            ft.Tab(text="Inicio",content=ft.Text("Contenido de Inicio")),
+            ft.Tab(text="Inicio",content=inicio_container),
+
             ft.Tab(text="Personajes", content=ft.Text("Contenido de Personajes")),  
             ft.Tab(text="Armas", content=ft.Text("Contenido de Armas")),
             ft.Tab(text="Vehiculos", content=ft.Text("Contenido de Vehiculos")),
@@ -33,12 +45,63 @@ def main(page: ft.Page):
     )
 
 
+    def cambiar_pagina(index):
+        page.clean()
+        page.add(page.navigation_bar)
+
+        if index ==0:
+
+            page.add(
+                ft.Column(
+                    [
+                        titulo, imagen_portada, tabs,
+                    ],
+                    alignment=ft.MainAxisAlignment.START,
+                    horizontal_alignment=ft.CrossAxisAlignment.CENTER
+                )
+            )
+
+        elif index == 1:
+            search_container = ft.Container(
+                content=ft.Column(
+                    [ft.TextField(label="Buscar"), ft.ElevatedButton("Buscar")],
+                    alignment=ft.MainAxisAlignment.START,
+                    horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+                ),
+                padding=20,
+                bgcolor=ft.Colors.WHITE,
+            )
+            page.add(search_container)
+
+        elif index == 2:
+            config_container = ft.Container(
+                content=ft.Column(
+                    [ft.Text("Ajustes"), ft.Slider(value=50)],
+                    alignment=ft.MainAxisAlignment.START,
+                    horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+                ),
+                padding=20,
+                bgcolor=ft.Colors.WHITE,
+            )
+            page.add(config_container)
+
+
+    page.navigation_bar = ft.NavigationBar(
+        destinations=[
+            ft.NavigationBarDestination(icon=ft.Icons.HOME, label="Inicio"),
+            ft.NavigationBarDestination(icon=ft.Icons.SEARCH, label="Buscar"),
+            ft.NavigationBarDestination(icon=ft.Icons.SETTINGS, label="Configuración")
+        ],
+        on_change=lambda e: cambiar_pagina(e.control.selected_index)
+    )
+
+
     page.add(
         ft.Column(
             [
                 titulo, 
                 imagen_portada,
-                tabs
+                tabs,
             ],
             alignment=ft.MainAxisAlignment.START,
             horizontal_alignment=ft.CrossAxisAlignment.CENTER
