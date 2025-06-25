e2f3980a4c7e8a12284ae0b4947f73560324d9d3
Gerry <lgmm-luis14@outlook.com>
2025-06-24 19:33:26 -0600

    portada y boton inicio 100% funcional

--- my-app/src/main.py
+++ my-app/src/main.py
@@ -5,9 +5,9 @@ def main(page: ft.Page):
     page.bgcolor = ft.Colors.BLACK
     page.title = "Enciclopedia de Gamer"
     page.scroll=True
-    titulo = ft.Text(
-        "Bienvenido a la Enciclopedia Gamer", size=30, color=ft.Colors.WHITE
-    )
+    modo_inicio = True
+    titulo = ft.Text("Bienvenido a la Enciclopedia Gamer", size=30, color=ft.Colors.WHITE)
+
 
     imagen_portada = ft.Image(
         src="https://www.elinformador.com.co/images/stories/general/2017/06-junio/20entre6.jpg",
@@ -15,6 +15,15 @@ def main(page: ft.Page):
         height=200,
         fit=ft.ImageFit.CONTAIN,
     )
+
+
+    halo_logo = ft.Image(
+        src="https://wallpaperaccess.com/full/50036.jpg",
+        width=600,
+        height=200,
+        fit=ft.ImageFit.COVER,
+    )
+
 
     inicio_container = ft.Container(
         content=ft.Column(
@@ -32,7 +41,12 @@ def main(page: ft.Page):
         selected_index=0,
         animation_duration=300,
         tabs=[
-            ft.Tab(text="Inicio",content=inicio_container),
+            ft.Tab(
+                text="Inicio",
+                content=ft.Column([
+                    ft.ElevatedButton("Volver a pantalla de inicio", on_click=lambda e: mostrar_pantalla_inicio())
+                ])
+            ),
 
             ft.Tab(text="Personajes", content=ft.Text("Contenido de Personajes")),  
             ft.Tab(text="Armas", content=ft.Text("Contenido de Armas")),
@@ -44,7 +58,20 @@ def main(page: ft.Page):
     )
 
 
+    page.navigation_bar = ft.NavigationBar(
+        destinations=[
+            ft.NavigationBarDestination(icon=ft.Icons.HOME, label="Inicio"),
+            ft.NavigationBarDestination(icon=ft.Icons.SEARCH, label="Buscar"),
+            ft.NavigationBarDestination(icon=ft.Icons.SETTINGS, label="Configuración")
+        ],
+        on_change=lambda e: cambiar_pagina(int(e.data))
+    )
+
+
     def cambiar_pagina(index):
+
+        if modo_inicio:
+            return
 
         page.clean()
         page.add(page.navigation_bar)
@@ -89,26 +116,67 @@ def main(page: ft.Page):
         page.update()
 
 
-    page.navigation_bar = ft.NavigationBar(
-        destinations=[
-            ft.NavigationBarDestination(icon=ft.Icons.HOME, label="Inicio"),
-            ft.NavigationBarDestination(icon=ft.Icons.SEARCH, label="Buscar"),
-            ft.NavigationBarDestination(icon=ft.Icons.SETTINGS, label="Configuración")
-        ],
-        on_change=lambda e: cambiar_pagina(int(e.data))
-    )
-
-
-    page.add(
-        ft.Column(
-            [
-                titulo, 
-                imagen_portada,
-                tabs,
-            ],
+    def alternar_inicio():
+        nonlocal modo_inicio
+        modo_inicio = False
+        page.clean()
+        page.add(page.navigation_bar)
+        page.add(ft.Column(
+            [titulo, imagen_portada, tabs],
             alignment=ft.MainAxisAlignment.START,
             horizontal_alignment=ft.CrossAxisAlignment.CENTER
+        ))
+        page.update()
+
+
+    def boton_principal_inicio():
+
+        return ft.ElevatedButton(
+            "Entrar",
+            on_click = lambda e: alternar_inicio(),
+            style=ft.ButtonStyle(
+                color={
+                    ft.ControlState.HOVERED: ft.Colors.WHITE,
+                    ft.ControlState.DEFAULT: ft.Colors.BLUE,
+                    ft.ControlState.FOCUSED: ft.Colors.BLACK,
+                },
+                bgcolor={ft.ControlState.FOCUSED: ft.Colors.PINK_200, "": ft.Colors.BLUE},
+                padding={ft.ControlState.HOVERED: 20},
+                overlay_color=ft.Colors.TRANSPARENT,
+                elevation={ft.ControlState.PRESSED: 0,
+                            ft.ControlState.DEFAULT: 1},
+                animation_duration=500,
+                side={
+                    ft.ControlState.DEFAULT: ft.BorderSide(2, ft.Colors.WHITE),
+                    ft.ControlState.HOVERED: ft.BorderSide(2, ft.Colors.BLUE),
+                },
+                shape={
+                    ft.ControlState.HOVERED: ft.RoundedRectangleBorder(radius=20),
+                    ft.ControlState.DEFAULT: ft.RoundedRectangleBorder(radius=2),
+                },
+            ),
         )
-    )
+
+
+    def mostrar_pantalla_inicio():
+        nonlocal modo_inicio
+        modo_inicio = True
+
+        page.clean()
+        page.add(
+            ft.Column(
+                [
+                    ft.Row(
+                    [halo_logo,
+                    boton_principal_inicio()],
+                    )
+                ],
+                alignment=ft.MainAxisAlignment.START,
+                horizontal_alignment=ft.CrossAxisAlignment.CENTER
+            )
+        )
+        page.update()
+    
+    mostrar_pantalla_inicio()
 
 ft.app(target=main)
